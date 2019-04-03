861b4c3e5f195cf84dec0121b3ea509d90703c5d
Edmilson Silva da Hora <edmilson_hora@yahoo.com.br>
2019-04-03 14:47:46 -0300

    comitando

--- ConsoleAppFunc/Program.cs
+++ ConsoleAppFunc/Program.cs
@@ -12,11 +12,17 @@ namespace ConsoleAppFunc
             var robo3 = new Conferidor();
 
             var coordenador = new Coordenador();
-            coordenador.Robo1 = robo1.Transportar;
-            coordenador.Robo2 = robo2.Desempacotar;
-            coordenador.Robo3 = robo3.Conferir;
+            coordenador.RoboTransportador = robo1;
+            coordenador.RoboDesempacotador = robo2;
+            coordenador.RoboConferidor = robo3;
 
-            Task task = Task.Factory.StartNew(() => coordenador.Executar("Pacote 1", 1000));
+            //coordenador.Executar(1000, "Pacote 1");
+
+            //coordenador.Robo1 = robo1.Transportar;
+            //coordenador.Robo2 = robo2.Desempacotar;
+            //coordenador.Robo3 = robo3.Conferir;
+
+            Task task = Task.Factory.StartNew(() => coordenador.Executar(1000, "Pacote 1"));
 
             //Task task2 = Task.Factory.StartNew(() => coordenador.Executar("Pacote 2", 2000));
 
@@ -35,6 +41,10 @@ namespace ConsoleAppFunc
         public Func<Coordenador, string, int, bool> Robo2 { get; set; }
         public Func<Coordenador, string, int, bool> Robo3 { get; set; }
 
+        public Transportador RoboTransportador { get; set; }
+        public Desempacotador RoboDesempacotador { get; set; }
+        public Conferidor RoboConferidor { get; set; }
+
 
         public void Executar(string mensagem, int intervalo)
         {
@@ -51,6 +61,22 @@ namespace ConsoleAppFunc
             }
 
         }
+
+        public void Executar(int intervalo, string mensagem)
+        {
+            Console.WriteLine($"==={mensagem}===");
+
+            var r1 = RoboTransportador.Transportar(mensagem, intervalo);
+            if (r1)
+            {
+                var r2 = RoboDesempacotador.Desempacotar(mensagem, intervalo);
+                if (r2)
+                {
+                    var r3 = RoboConferidor.Conferir(this, mensagem, intervalo);
+                }
+            }
+
+        }
     }
 
     class Transportador
@@ -61,11 +87,25 @@ namespace ConsoleAppFunc
             System.Threading.Thread.Sleep(intervalo);
             return true;
         }
+
+        public bool Transportar(string mensagem, int intervalo)
+        {
+            Console.WriteLine($"[{mensagem}] - Transportando...");
+            System.Threading.Thread.Sleep(intervalo);
+            return true;
+        }
     }
 
     class Desempacotador
     {
         public bool Desempacotar(Coordenador c, string mensagem, int intervalo)
+        {
+            Console.WriteLine($"[{mensagem}] - Desempacotando...");
+            System.Threading.Thread.Sleep(intervalo);
+            return true;
+        }
+
+        public bool Desempacotar(string mensagem, int intervalo)
         {
             Console.WriteLine($"[{mensagem}] - Desempacotando...");
             System.Threading.Thread.Sleep(intervalo);
@@ -82,8 +122,11 @@ namespace ConsoleAppFunc
             Console.WriteLine($"[{mensagem}] - Conferindo...");
             System.Threading.Thread.Sleep(intervalo);
             numerador++;
-            c.Executar("Pacote " + numerador, intervalo);
+            c.Executar(intervalo, "Pacote " + numerador);
+            //c.Executar("Pacote " + numerador, intervalo);
             return true;
         }
+
+       
     }
 }
